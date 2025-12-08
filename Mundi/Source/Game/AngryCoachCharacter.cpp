5c80ff66975e4aca1f013e7efbc6c70aba2579af
g0311 <gsc0815@naver.com>
2025-12-08 23:29:52 +0900

    feat: add camera shake when hit

--- Mundi/Source/Game/AngryCoachCharacter.cpp
+++ Mundi/Source/Game/AngryCoachCharacter.cpp
@@ -20,6 +20,7 @@
 #include "Source/Runtime/Core/Misc/PathUtils.h"
 #include "CloakAccessoryActor.h" 
 #include "FAudioDevice.h"
+#include "PlayerCameraManager.h"
 
 AAngryCoachCharacter::AAngryCoachCharacter()
 {
@@ -416,6 +417,10 @@ float AAngryCoachCharacter::TakeDamage(float DamageAmount, const FHitResult& Hit
 	
 	// 피해량을 감소시키는 요인이 있다면 감도된 피해량 적용	
 	float ActualDamage = DamageAmount;
+
+	GWorld->GetPlayerCameraManager()->StartCameraShake(
+		0.3, 0.3, 0.3, DamageAmount * 2
+		);
 
 	if (ActualDamage < 10.f)
 	{
